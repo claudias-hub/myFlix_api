1bdebfa54d7a2ca35782dd237114c498fd6e0fb7
claudias-hub <c_oka82@hotmail.com>
2025-05-06 13:49:26 +0200

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcrypt');
 
 const { Movie, User } = require("./models");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
 
